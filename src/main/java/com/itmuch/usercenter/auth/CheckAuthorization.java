ca810c7326bbef94a8a5543a945f06af932396f1
guanqing <guanqinghz@163.com>
2021-12-09 20:50:45 +0800

    AOP实现用户权限验证

--- src/main/java/com/itmuch/usercenter/auth/CheckAuthorization.java
+++ src/main/java/com/itmuch/usercenter/auth/CheckAuthorization.java
@@ -2,7 +2,11 @@ package com.itmuch.usercenter.auth;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-
+/**
+ * 鉴权注解
+ * @Author guanqing
+ * @Date 2021/12/9 20:50
+ **/
 @Retention(RetentionPolicy.RUNTIME)
 public @interface CheckAuthorization {
     String value();
--- src/main/java/com/itmuch/usercenter/auth/CheckLogin.java
+++ src/main/java/com/itmuch/usercenter/auth/CheckLogin.java
@@ -1,4 +1,9 @@
 package com.itmuch.usercenter.auth;
 
+/**
+ * 验证登录注解
+ * @Author guanqing
+ * @Date 2021/12/9 20:50
+ **/
 public @interface CheckLogin {
 }
